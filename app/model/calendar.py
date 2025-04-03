984e56890b516ac37e457894d4f79975f2b9737a
Juan Esteban Correa <correaguzmanjuanesteban@gmail.com>
2025-04-02 21:27:10 -0500

    se hace la implementacion de la primera clase

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -5,14 +5,16 @@ from typing import ClassVar
 from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
     reminder_not_found_error, slot_not_available_error
 
+@dataclass
 
-# TODO: Implement Reminder class here
+class Reminder:
+    EMAIL: ClassVar[str] = "email"
+    SYSTEM: ClassVar[str] = "system"
+    date_time: datetime
+    type: str = EMAIL
 
+    def __str__(self)-> str:
+        return f"Reminder on {self.date_time} of type {self.type}"
 
-# TODO: Implement Event class here
+class Event:
 
-
-# TODO: Implement Day class here
-
-
-# TODO: Implement Calendar class here
